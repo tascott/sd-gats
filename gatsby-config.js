8ce5bf79040fc87b6b8a6cbe13af40514b848528
TomScott <contact@tascott.co.uk>
2021-05-12 22:29:37 +0100

    Remove google analytics temporarily

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,12 +12,6 @@ module.exports = {
     "gatsby-plugin-styled-components",
     "gatsby-plugin-gatsby-cloud",
     "gatsby-plugin-image",
-    {
-      resolve: "gatsby-plugin-google-analytics",
-      options: {
-        trackingId: "",
-      },
-    },
     "gatsby-plugin-react-helmet",
     {
       resolve: "gatsby-plugin-manifest",
